dd1d121589467673149a962ee9a732f65c5138b4
GOKUL <71809441+Gokul-03@users.noreply.github.com>
2021-08-16 09:10:37 +0530

    Add files via upload

---
+++ firstproject/src/com/practice/Main.java
@@ -0,0 +1,20 @@
+package com.practice;
+//primary data types
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("hello world");//this is the command to print anything in java
+        int age=25;
+        double decimal=35.6;
+        char letter='r';
+        String name="hi! this is gokul,its my frst java program";
+        boolean yesorno=true;//we can only store either true or false(anyone)
+        System.out.println(age);
+        System.out.println(decimal);
+        System.out.println(letter);
+        System.out.println(name);
+        System.out.println(yesorno);
+    }
+}
+
