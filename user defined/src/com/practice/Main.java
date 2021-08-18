e9a306500023f887fcaae943c1e2ede16f1fd9dc
GOKUL <71809441+Gokul-03@users.noreply.github.com>
2021-08-18 09:43:08 +0530

    Add files via upload
    input from user

---
+++ user defined/src/com/practice/Main.java
@@ -0,0 +1,12 @@
+package com.practice;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int age=scanner.nextInt();
+        System.out.println("Your age is :"+age);
+    }
+}
